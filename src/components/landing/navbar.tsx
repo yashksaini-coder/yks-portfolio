0c029b461472cd20a72ad5640f2e42f399a558f6
yashksaini-coder <ys3853428@gmail.com>
2025-05-29 03:02:29 +0530

    navbar mid done

--- src/components/landing/navbar.tsx
+++ src/components/landing/navbar.tsx
@@ -6,46 +6,32 @@ import React from "react";
 import ThemeToggler from "@/components/theme/toggler";
 
 const links = [
-  { href: "/", text: "Home", key: "h" },
-  { href: "/w", text: "writings", key: "w" },
-  { href: "/projects", text: "projects", key: "p" },
-  { href: "/skills", text: "skills", key: "s" },
+  { href: "/", text: "Home" },
+  { href: "/w", text: "Writings" },
+  { href: "/projects", text: "Projects" },
+  { href: "/skills", text: "Skills" },
 ];
 
 export default function Navbar() {
   const pathname = usePathname();
   const router = useRouter();
 
-  const handleKeyPress = (event: KeyboardEvent) => {
-    const key = event.key.toLowerCase();
-    const link = links.find((link) => link.key === key);
-    if (link) {
-      router.push(link.href, { scroll: true });
-    }
-  };
-
-  React.useEffect(() => {
-    document.addEventListener("keypress", handleKeyPress);
-    return () => {
-      document.removeEventListener("keypress", handleKeyPress);
-    };
-  });
-
   return (
     <div className="w-full flex justify-center">
-      <nav className="flex max-w-3xl w-full px-4 py-6 text-sm text-muted-foreground items-center justify-between">
+      <nav className="flex max-w-5xl w-full px-4 py-6 text-sm text-muted-foreground items-center justify-between">
         <div className="flex items-center gap-6">
-          {links.map(({ href, text, key }) => (
+          {links.map(({ href, text }) => (
             <h4
-              className="hover:text-violet-500 sm:text-sm text-xs duration-300 transition-colors"
-              key={key}
+              className={`hover:text-violet-500 sm:text-sm text-xs duration-300 transition-colors ${
+                pathname === href ? "text-violet-500" : ""
+              }`}
+              key={href}
             >
-              <Link href={href}>
-                <span className={`${pathname === href && "text-violet-500"}`}>
-                  [{key}]
-                </span>{" "}
-                {text}
-              </Link>
+              <div className={`px-2 py-0.5 ${pathname === href ? 'border-2 border-violet-800 text-white' : 'hover:bg-violet-500/10'}`}>
+                <Link href={href} className="flex items-center gap-4 font-medium text-base">
+                  {text}
+                </Link>
+              </div>
             </h4>
           ))}
         </div>
