10f2a8d5a37f8aaf841f24f9eb6a9dfe1a858f15
yashksaini-coder <ys3853428@gmail.com>
2025-06-14 03:37:40 +0530

    undo markdown render component

--- src/components/Preview.tsx
+++ src/components/Preview.tsx
@@ -1,8 +1,9 @@
 import React from "react";
 import { HyperText } from "@/components/magicui/hyper-text";
 import Description from "./common/description";
-import CustomATag from "./CustomATag";
-
+import { LinkItem } from "./custom/LinkItem";
+import { Github, GithubIcon } from "lucide-react";
+import GitHub from "./SVG/Github";
 export function Preview() {
   return (
     <div>
@@ -13,13 +14,9 @@ export function Preview() {
         </h2>
       </div>
       <div className="my-4">
-        <Description className="text-slate-200 md:text-[20px] text-sm align-middle">
-          Hi, I'm Yash K. Saini a software developer who loves Open Source Contribution. Really passionate about coding, building AI applications, low-level programming stuff too.&nbsp;I enjoy working with{" "} 
-          I have a background in Data Science and believe in the power of collaboration. Check out my work on{" "}
-          <CustomATag href="https://github.com/yashksaini/CrossPostHub">
-            CrossPostHub
-          </CustomATag>&#39;
-          <CustomATag href="https://dev.to/yashksaini">Dev.to</CustomATag>{" "}
+        <Description className="text-slate-300 md:text-xl flex text-sm font-normal">
+          Hi, I'm a software developer who loves <LinkItem icon={Github} href="https://github.com/yashksaini-coder" content={"Open Source"} />. Taught myself code. Really passionate about coding, building AI applications, low-level programming stuff too.
+          I enjoy working with{" "}
         </Description>
       </div>
     </div>
